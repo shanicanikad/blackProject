2840c976b0482aadcf44904dc3111ad6f670fd6d
SHANICA_DENIS <shanicanikad@gmail.com>
2021-12-01 16:21:58 -0500

    fix2

--- db/connectionM.js
+++ db/connectionM.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-let mongoURI = "mongodb://shanicanikad1:shanicanikad97@cluster0-shard-00-00.ozamz.mongodb.net:27017,cluster0-shard-00-01.ozamz.mongodb.net:27017,cluster0-shard-00-02.ozamz.mongodb.net:27017/blackProject?ssl=true&replicaSet=atlas-lvq8ct-shard-0&authSource=admin&retryWrites=true&w=majority";
+let mongoURI = "";
 
 if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.DB_URL;
